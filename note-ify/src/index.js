f1f4a8705cbb73458859933f18cc7e998166b6c0
Usman Javed <ujaved931@gmail.com>
2018-05-31 02:33:04 +0500

    Added Boilerplate Redux Router

--- note-ify/src/index.js
+++ note-ify/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const dummyreducer = (state = [], action) => state;
+
+const store = createStore(dummyreducer);
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+        </Provider>
+    )
+    , document.getElementById('root'));
 registerServiceWorker();
