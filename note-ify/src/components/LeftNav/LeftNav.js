7045b3f4bd9c0ff82028d10c5da509e0e17405e8
Usman Javed <ujaved931@gmail.com>
2018-05-31 05:19:50 +0500

    Styled navigation bar styled with buttons

--- note-ify/src/components/LeftNav/LeftNav.js
+++ note-ify/src/components/LeftNav/LeftNav.js
@@ -4,7 +4,13 @@ class LeftNav extends Component {
     render() {
         return (
             <div className='nav_container'>
-            EditNote PLACEHOLDER 
+                <div>
+                    <h1 className='nav_header'>Lambda Notes</h1>
+                </div>
+                <div classNames='nav_buttons_container'>
+                    <div className='nav_button'>Veiw Your Notes</div>
+                    <div className='nav_button'>+ Create Your Notes</div>
+                </div>
             </div>
         );
     }
