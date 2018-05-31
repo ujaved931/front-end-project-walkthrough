c943b629d3ebb3c24a792f1e040589a7d465b222
Usman Javed <ujaved931@gmail.com>
2018-06-01 01:26:17 +0500

    Finished styling main-content added color added dimensions line-spacing font close to design files

--- note-ify/src/components/MainContent/MainContent.js
+++ note-ify/src/components/MainContent/MainContent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './index.css'
+import NotesList from '../NotesList/NotesList';
+
+
 class MainContent extends Component {
     render() {
         return (
             <div className='main_container'>
-                MainContent PLACEHOLDER
+                <NotesList />
             </div>
         );
     }
--- note-ify/src/components/NotesList/NotesList.js
+++ note-ify/src/components/NotesList/NotesList.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import './index.css'
+
+
 class NotesList extends Component {
     render() {
         return (
-            <div>
-                NoteList PLACEHOLDER
+            <div className='notesList_container'>
+                <div><h3 className='content_header'>Your Notes:</h3></div>
+                <div className='notesList'>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                    <div className='note'>
+                        <h4>Note Title</h4>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ornare dolor a enim luctus, sed efficitur leo ornare. Donec malesuada suscipit nisl, eu condimentum augue tempus eget. Cras semper luctus lorem a tincidunt. Sed pulvinar commodo sem, eget finibus nulla mattis sit amet. Cras odio tortor, egestas a auctor sed, volutpat quis justo. Etiam efficitur turpis at mauris dignissim efficitur. Vestibulum et odio lectus. Praesent lobortis commodo massa, sed porttitor nunc semper tincidunt. Vivamus in dolor pharetra, euismod eros vitae, porta turpis. Etiam pharetra tempus lacus. Morbi ultrices felis quis ante imperdiet molestie sed vel metus.</p>
+                    </div>
+                </div>
             </div>
         );
     }
