690f1c1b29d21d9da08bbba1a3a11411421c1fd8
Usman Javed <ujaved931@gmail.com>
2018-06-02 02:45:55 +0500

    Basic MVP done working on redux

--- note-ify/src/index.js
+++ note-ify/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const dummyreducer = (state = [], action) => state;
+
 
 const store = createStore(dummyreducer);
 
